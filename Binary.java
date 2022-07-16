7aed1f9f7a59853efe1629f8f7a88d05fb1bdea7
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-16 13:12:24 +0200

    2nd

--- Binary.java
+++ Binary.java
@@ -6,6 +6,20 @@ public class Binary {
         byte age = 127;
         age = (byte) (age+1);
         System.out.println(age);
+
+        int a = 5; // 0101
+        int b = 6; // 0110
+
+        int c = a | b; // 0111
+        System.out.println(c);
+        int d = a & b; // 0100
+        System.out.println(d);
+
+        int e = a ^ b; // 0011
+        System.out.println(e);
+
+        int f = ~ a; // 1010
+        System.out.println(f);
     }
 
 
