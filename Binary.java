fc2d29e3455b208e738c9102558df8da8069dd55
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-16 12:02:17 +0200

    1st

--- Binary.java
+++ Binary.java
@@ -1,2 +1,13 @@
+import com.sun.tools.javac.Main;
+
 public class Binary {
+
+    public static void main(String[] args) {
+        byte age = 127;
+        age = (byte) (age+1);
+        System.out.println(age);
+    }
+
+
+
 }
