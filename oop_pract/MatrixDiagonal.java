b16875a78d1e19f9a6ea4948e1a8afe90cfa8350
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-16 16:46:37 +0200

    1st

---
+++ oop_pract/MatrixDiagonal.java
@@ -0,0 +1,11 @@
+package oop_pract;
+
+public class MatrixDiagonal {
+  public int[] getDiagonal(int[][] matrix) {
+    return null;
+  }
+
+  public int[] getCounterDiagonal(int[][] matrix) {
+    return null;
+  }
+}
